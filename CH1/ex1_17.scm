6bd6b59102634c654ccf2816e9abf1853ae41325
chouxi <qizheng1993hit@gmail.com>
2019-01-21 00:13:03 -0800

    update 1.17 and 1.18

--- CH1/ex1_17.scm
+++ CH1/ex1_17.scm
@@ -1,9 +1,7 @@
 (define (double n) (+ n n))
 (define (halve n) (/ n 2))
 
-(define (fast-add b n)
-  (define (iter a b n)
-    (cond ((= n 0) a)
-          ((even? n) (iter a (double b) (halve n)))
-          (else (iter (+ a b) b (- n 1)))))
-  (iter 0 b n))
+(define (fast-multi b n)
+  (cond ((= n 0) 0)
+        ((even? n) (double (fast-multi b (halve n))))
+        (else (+ b (fast-multi b (- n 1))))))
---
+++ CH1/ex1_18.scm
@@ -0,0 +1,9 @@
+(define (double n) (+ n n))
+(define (halve n) (/ n 2))
+
+(define (fast-multi b n)
+  (define (iter a b n)
+    (cond ((= n 0) a)
+          ((even? n) (iter a (double b) (halve n)))
+          (else (iter (+ a b) b (- n 1)))))
+  (iter 0 b n))
